73959e6b91ba8f0cf905a1d52fb45905ef290541
Inrix <Inrixia@gmail.com>
2020-12-21 00:02:21 +1300

    Added toBeOneOf and changed jest exports to single extends object

--- src/jest/index.ts
+++ src/jest/index.ts
@@ -7,43 +7,49 @@ declare global {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         interface Matchers<R> {
 			toMatchFormat: (format: Types) => CustomMatcherResult,
-			childrenToMatchFormat: (format: Types) => jest.CustomMatcherResult
+			childrenToMatchFormat: (format: Types) => jest.CustomMatcherResult,
+			toBeOneOf(items: Array<unknown>): jest.CustomMatcherResult;
         }
     }
 }
 
-/**
- * Jest expects extend function for checking an object match a specific format. Uses @inrixia/helpers/object.deepTypeCompare
- */
-export const toMatchFormat = (object: Record<string, unknown>, format: Types): jest.CustomMatcherResult => {
-	const matches = deepTypeCompare(object, format);
-	if (typeof matches !== "boolean") return {
-		message: () => `${matches.location} does not match expected Format\n${matches.received} != ${matches.expectedType}`,
-		pass: false,
-	};
-	return {
-		message: () => "matches expected Format",
-		pass: true,
-	};
-};
-
-/**
- * Jest expects extend function for checking an Array's children match a specific format. Uses @inrixia/helpers/object.deepTypeCompare
- */
-export const childrenToMatchFormat = (array: Array<Record<string, unknown>>, format: Types): jest.CustomMatcherResult => {
-	if (!Array.isArray(array)) return {
-		message: () => `${iObj(array)} type is not Array`,
-		pass: false,
-	};
-	for (const object of array) {
+export default {
+	toBeOneOf: (received: unknown, items: Array<unknown>): jest.CustomMatcherResult => ({
+		message: () => `expected ${received} to be contained in array [${items}]`,
+		pass: items.includes(received),
+	}),
+	/**
+	 * Jest expects extend function for checking an object match a specific format. Uses @inrixia/helpers/object.deepTypeCompare
+	 */
+	toMatchFormat: (object: Record<string, unknown>, format: Types): jest.CustomMatcherResult => {
 		const matches = deepTypeCompare(object, format);
 		if (typeof matches !== "boolean") return {
 			message: () => `${matches.location} does not match expected Format\n${matches.received} != ${matches.expectedType}`,
 			pass: false,
 		};
+		return {
+			message: () => "matches expected Format",
+			pass: true,
+		};
+	},
+	/**
+	 * Jest expects extend function for checking an Array's children match a specific format. Uses @inrixia/helpers/object.deepTypeCompare
+	 */
+	childrenToMatchFormat: (array: Array<Record<string, unknown>>, format: Types): jest.CustomMatcherResult => {
+		if (!Array.isArray(array)) return {
+			message: () => `${iObj(array)} type is not Array`,
+			pass: false,
+		};
+		for (const object of array) {
+			const matches = deepTypeCompare(object, format);
+			if (typeof matches !== "boolean") return {
+				message: () => `${matches.location} does not match expected Format\n${matches.received} != ${matches.expectedType}`,
+				pass: false,
+			};
+		}
+		return {
+			message: () => "children match expected Format",
+			pass: true,
+		};
 	}
-	return {
-		message: () => "children match expected Format",
-		pass: true,
-	};
 };

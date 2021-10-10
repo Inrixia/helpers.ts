3396044a79bb958e148ab56f3130fb28b8e8461b
Inrix <Inrixia@gmail.com>
2021-10-10 13:29:38 +1300

    Added react helpers & react/toBase64

---
+++ src/react/index.ts
@@ -0,0 +1,11 @@
+export const toBase64 = async (file: File): Promise<string | ArrayBuffer> => {
+	const base64Image = await new Promise<string | ArrayBuffer | null>((resolve, reject) => {
+		const reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = () => resolve(reader.result);
+		reader.onerror = reject;
+	}).catch((err) => err);
+	if (base64Image instanceof Error) throw new Error(`Failed to convert image ${file.name} to base64.\n${base64Image.message}`);
+	if (base64Image === undefined || base64Image === null) throw new Error(`Failed to convert image ${file.name} to base64.`);
+	return base64Image;
+};

e56d78a0e3e67efb31338b4daecb20880820a45c
Inrixia <Inrixia@gmail.com>
2025-04-04 14:26:22 +1300

    Swap to single index

--- src/math/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./angleDifference.js";
-export * from "./nPad.js";
-export * from "./qRnd.js";
-export * from "./to360.js";
--- src/ts/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./isKeyOf.js";
-export * from "./isTypeOf.js";
-export * from "./math.js";
-export * from "./numRange.js";
-export * from "./typeOf.js";
-export * from "./types.js";
